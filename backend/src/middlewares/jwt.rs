6ff82e91f4dd0920ebccdddd435f2ef173583a61
Wilovy09 <srwilovy09@gmail.com>
2024-09-11 04:35:59 -0600

    :sparkles: Se agrega `middleware` para funcionar como validador

--- backend/src/middlewares/jwt.rs
+++ backend/src/middlewares/jwt.rs
@@ -3,6 +3,12 @@ use dotenv::dotenv;
 use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
 use serde::{Deserialize, Serialize};
 use std::env;
+use crate::AppState;
+use actix_web::{dev::ServiceRequest,error, Error};
+use actix_web::web::Data;
+use actix_web_httpauth::extractors::bearer::BearerAuth;
+use sqlx::Error as SqlxError;
+use actix_web_grants::authorities::AttachAuthorities;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TokenStruct {
@@ -59,5 +65,52 @@ pub fn validate_token(token: String) -> Result<Claims, jsonwebtoken::errors::Err
     match result {
         Ok(c) => Ok(c.claims),
         Err(e) => Err(e),
+    }
+}
+
+pub async fn validator(
+    req: ServiceRequest,
+    credenciales: Option<BearerAuth>,
+) -> Result<ServiceRequest, (Error, ServiceRequest)> {
+    let Some(credenciales) = credenciales else {
+        return Err((error::ErrorBadRequest("No se especificó el token"), req));
+    };
+    let token = credenciales.token();
+    let state = match req.app_data::<Data<AppState>>() {
+        Some(data) => data,
+        None => return Err((error::ErrorInternalServerError("No se pudo obtener el estado."), req)),
+    };
+    match validate_token(token.to_owned()) {
+        Ok(token) => {
+            if token.token_type == "refresh" {
+                return Err((error::ErrorUnauthorized("Token inválido."), req));
+            }
+            match sqlx::query!("SELECT role_id FROM User WHERE id = $1", token.user_id)
+                .fetch_one(&state.db)
+                .await
+            {
+                Ok(record) => {
+                    let role_id = record.role_id;
+                    match role_id {
+                        1 => req.attach(vec!["Administrador".to_string()]),
+                        2 => req.attach(vec!["Cliente".to_string()]),
+                        _ => req.attach(vec!["Usuario".to_string()]),
+                    }
+                    Ok(req)
+                }
+                Err(SqlxError::RowNotFound) => {
+                    Err((error::ErrorNotFound("Usuario no encontrado."), req))
+                }
+                Err(_) => {
+                    Err((
+                        error::ErrorInternalServerError("Error al consultar la base de datos."),
+                        req,
+                    ))
+                }
+            }
+        }
+        Err(_) => {
+            Err((error::ErrorForbidden("No tiene acceso."), req))
+        }
     }
 }
