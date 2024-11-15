844da19cbf18e01bca74952e687d6700d61ede82
JonathanACO <jonathanandresco@gmail.com>
2024-11-14 23:22:44 -0600

    se agregan rutas dentro del layout

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -1,13 +1,21 @@
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: () => import("../pages/Home.vue"),
-  },
-  {
     path: "/auth",
     name: "Authentication",
     component: () => import("../pages/AuthenticationPage/AuthPage.vue"),
+  },
+  {
+    path: "/",
+    name: "Layout",
+    meta: { requiresAuth: true },
+    //component: ()=> import ("../layouts/MainLayout.vue")
+    children: [
+      {
+        path: "",
+        name: "Home",
+        component: () => import("../pages/Home.vue"),
+      },
+    ],
   },
 ];
 
