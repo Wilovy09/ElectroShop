dba3b0deea4a22b4c9d74d02229427a64c9a2445
JonathanACO <jonathanandresco@gmail.com>
2024-11-17 18:43:15 -0600

    se cambia de directorio

---
+++ frontend/src/pages/HomePage/Home.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ProductCard from "./components/ProductCard.vue";
+</script>
+<template>
+  <div class="flex w-full justify-center">
+    <div class="flex max-w-max flex-wrap gap-8">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
+  </div>
+</template>
