82fd1fbd1e3a98c43816849af157d7ca0fb3cb41
Wilovy09 <srwilovy09@gmail.com>
2024-11-18 03:16:57 -0600

    :sparkles: Add a

--- frontend/src/pages/HomePage/Home.vue
+++ frontend/src/pages/HomePage/Home.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import ProductCard from "./components/ProductCard.vue";
+import { fakesProducts } from "../../entities/Product";
 </script>
+
 <template>
-  <div class="flex justify-center w-full">
-    <div class="grid grid-cols-2 xl:grid-cols-3 max-w-max gap-8">
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+    <div class="flex justify-center w-full">
+        <div class="grid grid-cols-2 xl:grid-cols-3 max-w-max gap-8">
+            <ProductCard
+                v-for="product in fakesProducts"
+                :key="product.id"
+                :product="product"
+            />
+        </div>
     </div>
-  </div>
 </template>
