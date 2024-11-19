ea751837023251db210088062f184956eda5db77
JonathanACO <jonathanandresco@gmail.com>
2024-11-19 03:30:31 -0600

    componente loader

---
+++ frontend/src/components/VLoader.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="w-full h-full flex justify-center items-center z-50">
+        <div class="max-w-14 h-full w-full max-h-14 rounded-full border-4 border-t-white border-stone-600 animate-spin"></div>
+    </div>
+</template>
